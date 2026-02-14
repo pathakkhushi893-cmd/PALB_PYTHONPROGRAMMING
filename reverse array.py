a56835fd9acc694cfd64c13183cc2e94a13c943b
pathakkhushi893-cmd <pathakkhushi893@gmail.com>
2026-02-14 20:03:07 +0530

    Create reverse array.py

---
+++ reverse array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseArray(self, arr):
+        left = 0
+        right = len(arr) - 1
+        
+        while left < right:
+            # Swap elements
+            arr[left], arr[right] = arr[right], arr[left]
+            
+            left += 1
+            right -= 1

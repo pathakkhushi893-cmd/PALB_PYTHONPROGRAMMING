30eb8c0b9aaa3c7ec30fc5a6f7df3e635d0745d9
pathakkhushi893-cmd <pathakkhushi893@gmail.com>
2026-02-14 20:21:09 +0530

    Create Min and Max in Array.py

---
+++ Min and Max in Array.py
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> getMinMax(vector<int> &arr) {
+        int minimum = arr[0];
+        int maximum = arr[0];
+        
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] < minimum) {
+                minimum = arr[i];
+            }
+            if (arr[i] > maximum) {
+                maximum = arr[i];
+            }
+        }
+        
+        return {minimum, maximum};
+    }
+};
+

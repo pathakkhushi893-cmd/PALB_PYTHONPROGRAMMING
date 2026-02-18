cf1bb9dcf97465ec6aeabb2c3d50188d1115e755
pathakkhushi893-cmd <pathakkhushi893@gmail.com>
2026-02-18 19:59:26 +0530

    Create jump game ii.py

---
+++ jump game ii.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        for i in range(len(nums) - 1):
+            # Update the farthest reachable index
+            farthest = max(farthest, i + nums[i])
+
+        
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps

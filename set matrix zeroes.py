57067edd93fbb37a3d673f94ba55296634f04066
pathakkhushi893-cmd <pathakkhushi893@gmail.com>
2026-02-18 20:12:15 +0530

    Create set matrix zeroes.py

---
+++ set matrix zeroes.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        
+        m, n = len(matrix), len(matrix[0])
+
+        first_row_zero = any(matrix[0][j] == 0 for j in range(n))
+        first_col_zero = any(matrix[i][0] == 0 for i in range(m))
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, m):
+            if matrix[i][0] == 0:
+                for j in range(1, n):
+                    matrix[i][j] = 0
+
+        for j in range(1, n):
+            if matrix[0][j] == 0:
+                for i in range(1, m):
+                    matrix[i][j] = 0
+
+        if first_row_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if first_col_zero:
+            for i in range(m):
+                matrix[i][0] = 0
